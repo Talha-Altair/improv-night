268b83424d496bac7162f1ab7cd9b4aae397014c
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-05-19 23:03:59 +0530

    Updates for 2021-05-19

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def message():
     socketio.emit('completed',event)
 
 if __name__ == "__main__":
-    socketio.run(app, debug = True,host="0.0.0.0",port = 8080)
+    socketio.run(app, debug = True)
